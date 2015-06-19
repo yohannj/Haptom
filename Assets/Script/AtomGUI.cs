5a57be0916fe52bfc66d1b5ea1706f7934932dd3
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-06-19 19:28:15 -0400

    Added a FalseGUI

---
+++ Assets/Script/AtomGUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtomGUI : MonoBehaviour
+{
+
+    void OnTriggerEnter (Collider other)
+    {
+        ((Behaviour) transform.parent.GetComponent("Halo")).enabled = true;
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        ((Behaviour)transform.parent.GetComponent("Halo")).enabled = false;
+    }
+}
