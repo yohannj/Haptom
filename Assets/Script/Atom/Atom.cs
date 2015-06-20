4eba0deb89b37b3db9b650a4cf804446b00fc4b1
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-06-20 16:34:50 -0400

    Creation of atom (no position nor material and such yet) with middle button of Falcon.

---
+++ Assets/Script/Atom/Atom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atom : MonoBehaviour {
+
+    string name;
+    int valence;
+    Renderer rend;
+
+    bool is_picked;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        is_picked = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void Set(string name, float scale, Material material, int valence)
+    {
+        this.name = name;
+        this.transform.localScale = new Vector3(scale, scale, scale);
+        this.rend.material = material;
+        this.valence = valence;
+    }
+}
--- Assets/Script/GUIs/AtomGUI.cs
+++ Assets/Script/GUIs/AtomGUI.cs
@@ -3,14 +3,55 @@ using System.Collections;
 
 public class AtomGUI : MonoBehaviour
 {
+    bool is_cursor_over;
+    bool atom_already_created;
+
+    void Start()
+    {
+        is_cursor_over = false;
+        atom_already_created = false;
+    }
+
+    void Update ()
+    {
+        if (is_cursor_over && !atom_already_created)
+        {
+            bool[] buttons;
+            FalconUnity.getFalconButtonStates(0, out buttons);
+
+            if (buttons[0])
+            {
+                atom_already_created = true;
+                GameObject new_atom;
+                var atom_name = transform.parent.name;
+
+
+                switch (atom_name)
+                {
+                    case "Calcium":
+                        new_atom = AtomManager.instance.SpawnCalcium();
+                        //new_atom.transform = Falcon.instance
+                        break;
+                    case "Carbon":
+                        new_atom = AtomManager.instance.SpawnCarbon();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 
     void OnTriggerEnter (Collider other)
     {
+        is_cursor_over = true;
         ((Behaviour) transform.parent.GetComponent("Halo")).enabled = true;
     }
 
     void OnTriggerExit (Collider other)
     {
+        is_cursor_over = false;
+        atom_already_created = false;
         ((Behaviour)transform.parent.GetComponent("Halo")).enabled = false;
     }
 }
