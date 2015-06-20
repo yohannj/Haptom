83ed2eda53fc6ac793818d66cbc30b0cbe0bca2f
Julien Apprioual <fgfgfgfg>
2015-06-20 14:14:52 -0400

    Atom prefab

---
+++ Assets/Script/SphereConstructor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereConstructor : MonoBehaviour {
+
+	public string type;
+	public Color _color;
+	public float scale = 1f;
+
+	// Use this for initialization
+	void Start () {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor("_Color", _color);
+
+		transform.localScale = new Vector3(scale, scale, scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
