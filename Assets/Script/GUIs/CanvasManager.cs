410984ffdf6f5ebefd9d8245844635e4ddee9e58
StanislasDescamps <stanislas.descamps@gmail.com>
2015-06-20 16:57:36 -0400

    time creation

---
+++ Assets/Script/GUIs/CanvasManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasManager : MonoBehaviour {
+
+    public Text TimeLabel, TimeValue, HintLabel, HintValue;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+        TimeValue.text = minutes + ":" + seconds;
+
+	}
+}
