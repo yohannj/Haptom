db9e9d44fc86f6d5fb5b12dd3f4f8f2bf2645bfd
Steven Thillier <s.thillier58@gmail.com>
2015-06-21 10:41:55 -0400

    Add pause GUI

--- Assets/Script/UIPauseMenu.cs
+++ Assets/Script/UIPauseMenu.cs
@@ -4,24 +4,23 @@ using System.Collections;
 public class UIPauseMenu : MonoBehaviour {
 	
 	private bool IsPaused = false;
+	Canvas canvas;
 
 	// Use this for initialization
 	void Start () {
-		Canvas canvas = GetComponent<Canvas>();
+		canvas = GetComponent<Canvas>();
 		canvas.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Canvas canvas = GetComponent<Canvas>();
-
-		if(Input.GetKeyDown("p")||Input.GetKeyDown(KeyCode.Escape) && !IsPaused)
+		if((Input.GetKeyDown("p")||Input.GetKeyDown(KeyCode.Escape)) && !IsPaused)
 		{
 			Time.timeScale = 0;
 			canvas.enabled = true;
 			IsPaused = true;
 		}
-		else if (Input.GetKeyDown("p")||Input.GetKeyDown(KeyCode.Escape)&& IsPaused)
+		else if ((Input.GetKeyDown("p")||Input.GetKeyDown(KeyCode.Escape))&& IsPaused)
 		{
 			Time.timeScale = 1;
 			canvas.enabled = false;
@@ -31,8 +30,7 @@ public class UIPauseMenu : MonoBehaviour {
 	}
 
 	public void Continue(){
-		Canvas canvas = GetComponent<Canvas>();
-		print ("ispaused :" + IsPaused);
+		Debug.Log("ispaused :" + IsPaused);
 		if (IsPaused)
 		{
 			Time.timeScale = 1;
