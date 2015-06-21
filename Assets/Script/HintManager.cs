ab06435fca7335c43f0a5d2cc74f62edcfcc9f57
StanislasDescamps <stanislas.descamps@gmail.com>
2015-06-21 17:29:42 -0400

    animation for opening

---
+++ Assets/Script/HintManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HintManager : MonoBehaviour {
+
+    public Hint CurrentHint;
+
+    // Use this for initialization
+    public void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.H))
+        {
+            ShowHint(CurrentHint);
+        }
+    }
+
+    public void ShowHint(Hint hint)
+    {
+        if (CurrentHint != null)
+            CurrentHint.IsOpen = false;
+
+            CurrentHint = hint;
+            CurrentHint.IsOpen = true;
+
+    }
+}
