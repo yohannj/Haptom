7ccb87bb060a8562142ebfe105613e3c733e267b
StanislasDescamps <stanislas.descamps@gmail.com>
2015-06-22 16:32:01 -0400

    Add panelSuccess and hide by default

---
+++ Assets/Script/GUIs/UISuccess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISuccess : MonoBehaviour {
+
+
+    public GameObject panel;
+    public CanvasGroup _canvasGroup;
+	// Use this for initialization
+	void Start () {
+        panel = GameObject.FindGameObjectWithTag("PanelSuccess");
+        panel.gameObject.SetActive(false);
+
+        _canvasGroup.alpha = 0;
+	}
+	
+	
+}
